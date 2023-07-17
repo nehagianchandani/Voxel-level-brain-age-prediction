03945df40e16d5e1b583899863243572b4449560
Neha Gianchandani <51573050+nehagianchandani@users.noreply.github.com>
2023-07-17 10:40:38 -0600

    Update training.py

--- ablation_experiments/seg_voxage/training.py
+++ ablation_experiments/seg_voxage/training.py
@@ -33,14 +33,11 @@ def train(train_loader, val_loader, model, optimizer, scheduler, max_epochs, roo
         print("Epoch ", epoch)
         print("Train:", end ="")
         if epoch < 50:
-            glob_coef = 1
-            vox_coef = 1
-        elif (epoch >=50) and (epoch<100):
-            glob_coef = 0.8
-            vox_coef =1.2
+            dice_coef = 50
+        elif epoch >=50 and epoch<100:
+            dice_coef = 10
         else:
-            glob_coef = 0.6
-            vox_coef =1.4
+            dice_coef = 5
 
         for step, batch in enumerate(train_loader):
             img, brain_mask, tissue_mask, age = (batch["img"].cuda(), batch["brain_mask"].cuda(),
@@ -50,9 +47,9 @@ def train(train_loader, val_loader, model, optimizer, scheduler, max_epochs, roo
        
             optimizer.zero_grad()
 
-            pred_tissue_mask, pred_glob_age, pred_voxel_age = model(brain_img)
+            pred_tissue_mask, pred_vox_age = model(brain_img)
 
-            loss = glob_coef*(global_mae_loss(pred_glob_age, age, brain_mask)) + vox_coef*(voxel_mae(pred_vox_age, age, root_dir, brain_mask))
+            loss = (dice_coef*loss_object(pred_tissue_mask,tissue_mask)) + voxel_mae(pred_vox_age, age, root_dir, brain_mask)
             loss.backward()
             train_loss += loss.item()
             optimizer.step()
@@ -64,46 +61,61 @@ def train(train_loader, val_loader, model, optimizer, scheduler, max_epochs, roo
         print()
         print("Val:", end ="")
         with torch.no_grad():
-                glob_mae_loss=0.0
-                vox_mae_loss = 0.0
+                mae_loss=0.0
                 for step, batch in enumerate(val_loader):
                     img, brain_mask, tissue_mask, age = (batch["img"].cuda(), batch["brain_mask"].cuda(),
                                             batch["seg_label"].cuda(), batch["age_label"].cuda())
                     brain_img = img*brain_mask
 
-                    pred_glob_age, pred_vox_age = model(brain_img)
+                    pred_tissue_mask, pred_vox_age = model(brain_img)
 
-                    loss = glob_coef * (global_mae_loss(pred_glob_age, age, brain_mask)) + vox_coef*(voxel_mae(pred_vox_age, age, root_dir, brain_mask))
+
+                    loss = (dice_coef*loss_object(pred_tissue_mask,tissue_mask)) + voxel_mae(pred_vox_age, age, root_dir, brain_mask)
                     val_loss += loss.item()
-
-                    glob_mae_loss += global_mae_loss(pred_glob_age, age, brain_mask)
-                    vox_mae_loss += voxel_mae(pred_vox_age, age, root_dir, brain_mask)
+                    tissue_mask = [post_seglabel(i) for i in decollate_batch(tissue_mask)]
+                    tissue_mask = pad_list_data_collate(tissue_mask)
+                    metric_object(y_pred=pred_tissue_mask, y=tissue_mask)
+                    mae_loss += voxel_mae(pred_vox_age, age, root_dir, brain_mask)
 
                     print("=", end = "")
                 print()
                 val_loss = val_loss/(step+1)
-                glob_mae_loss = glob_mae_loss/ (step+1)
-                vox_mae_loss = vox_mae_loss/ (step+1)
+                dice_metric = metric_object.aggregate().item()
+                metric_object.reset()
+                mae_loss = mae_loss/ (step+1)
+                #print(mae_loss)
+        if epoch % 100 == 0:
+                img = img.cpu()
+                pred_tissue_mask = pred_tissue_mask.cpu()
+                plt.figure()
+                plt.subplot(121)
+                plt.imshow(img.numpy()[0,0,32,:,:], cmap = "gray")
+                plt.subplot(122)
+                plt.imshow(img.numpy()[0,0,32,:,:], cmap = "gray")
+                plt.imshow(np.argmax(pred_tissue_mask.numpy(),axis = 1)[0,32,:,:], alpha = 0.4)
+                plt.savefig("./Figs1/val_sample_epoch_" + str(epoch) + ".png")
 
-
-        print("Training epoch ", epoch, ", train loss:", train_loss, ", val loss:", val_loss, ", val glob mae:", glob_mae_loss.item(), ", val vox mae:", vox_mae_loss.item())
+        print("Training epoch ", epoch, ", train loss:", train_loss, ", val loss:", val_loss, ", val dice score:", dice_metric, ", val mae score:", mae_loss.item())
         wandb.log({"train_loss": train_loss})
         wandb.log({"val_loss": val_loss})
-        wandb.log({"val_glob_mae": glob_mae_loss.item()})
-        wandb.log({"val_vox_mae": vox_mae_loss.item()})
+        wandb.log({"val_dice_metric": dice_metric})
+        wandb.log({"mae_voxel": mae_loss.item()})
         if epoch == 1:
             best_val_loss = val_loss
-
-
+            best_val_dice = dice_metric
+            best_mae_score = mae_loss.item()
         if val_loss < best_val_loss:
             print("Saving model")
             best_val_loss = val_loss
+            best_dice_metric = dice_metric
+            best_mae_score = mae_loss.item()
             state = {
                 'epoch': epoch,
                 'state_dict': model.state_dict(),
                 'optimizer': optimizer.state_dict()
             }
-            torch.save(state, root_dir + "/globage_voxage.pth")
+            #torch.save(model.state_dict(), root_dir + "/seg_age_network.pth")
+            torch.save(state, root_dir + "/seg_voxage_1.pth")
         scheduler.step()
         wandb.log({"epoch": epoch})
     return
